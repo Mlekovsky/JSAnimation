f8d45590fde41b0d9cc004117bcf24bdd0fc56c8
Damian <damian.smietana97@gmail.com>
2019-11-12 21:43:21 +0100

    Added fishes with logic, fixed sun display, added buttons with logic

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ var context;
 var repeater;
 var backgroundColor = "#88cdf2"; 
 var pile;
-var island; var islandHeight = 90; var islandWidth = 250; var islandColor="#dde077";
+var island; var islandHeight = 100; var islandWidth = 250; var islandColor="#dde077";
 var water; var waterHeight; var waterWidth; var waterColor = "#0324fc";
-var sun; var sunColor = "#f6fa00"; var opacityMinRange = 0.3; var sunMaxSize = 1.5; var sunRaysCount = 5;
+var sun; var sunColor = "#f6fa00"; var opacityMinRange = 0.9; var sunMaxSize = 1.1; var sunRaysCount = 5;
+var fishColor = "#de7a10"; var fishCount = 3; var fishEyeColor = "#f2356b "; //#57a0ff = niebieski
+const Directions = {
+    LEFT: 'left',
+    RIGHT: 'right'
+};
 
 function beginAnimation()
 {
@@ -13,7 +18,7 @@ function beginAnimation()
     context = canvas.getContext( "2d" );
 
     context.fillStyle = backgroundColor;
-    context.fillRect( 0, 0, canvas.width, canvas.height );
+    context.fillRect( 0, 0, canvas.width, canvas.height);
 
     //Initiailze Pile
     pile = new Pile();    
@@ -26,11 +31,11 @@ function beginAnimation()
 
     //Initialize Water
     waterWidth = canvas.width - islandWidth;
-    waterHeight = islandHeight - 30;
+    waterHeight = islandHeight - 20;
     water = new Water(waterHeight, waterWidth, waterColor);
     water.init();
     water.setReferencePoint(canvas.width - waterWidth, canvas.height - waterHeight);
-
+    water.initFishes();
     //Initialize Sun;
     sun = new Sun(sunColor, 30);
     sun.init();
@@ -40,6 +45,8 @@ function beginAnimation()
 
 function continueAnimation()
 {
+    context.fillStyle = backgroundColor;
+    context.fillRect( 0, 0, canvas.width, canvas.height );
     island.draw();
     pile.draw();
     water.draw();
@@ -141,10 +148,45 @@ function Water(height,width,color)
     this.X0 = 0;
     this.Y0 = 0;
 
+    this.fishes = [];
 
     this.init = function()
     {
         this.water = new Rect(this.color);
+    }
+
+    this.initFishes = function()
+    {
+        for(var i = 0; i < fishCount; i++)
+        {
+            var fish = new Fish(fishColor, 10,15,20,20);
+            fish.setReferencePoint(this.X0, this.Y0);
+            fish.init();
+            this.fishes.push(fish);
+        }
+    }
+
+    this.drawFishes = function()
+    {
+        for(var i = 0; i < fishCount; i++)
+        {
+            var fish = this.fishes[i];
+            fish.move();
+            fish.draw();
+        }
+    }
+
+    this.removeFish = function()
+    {
+        this.fishes.pop();
+    }
+
+    this.addFish = function()
+    {
+        var fish = new Fish(fishColor, 10,15,20,20);
+            fish.setReferencePoint(this.X0, this.Y0);
+            fish.init();
+            this.fishes.push(fish);
     }
 
     this.draw = function()
@@ -153,6 +195,7 @@ function Water(height,width,color)
         context.transform(this.width,0,0,this.height, this.water.X0, this.water.Y0);
         this.water.display();
         context.restore();
+        this.drawFishes();
     }
 
     this.setReferencePoint = function(x,y)
@@ -163,14 +206,136 @@ function Water(height,width,color)
     }
 }
 
-function Fish()
+function Fish(color,bodySize, finLength, finHeight, eyeSize)
 {
-//TODO:Fill with logic
+    this.X0 = 0;
+    this.Y0 = 0;
+    this.color = color;
+    this.direction = Directions.RIGHT;
+    this.finLength = finLength;
+    this.bodySize = bodySize;
+    this.finHeight = finHeight;
+    this.eyeSize = eyeSize;
+
+    this.posX = 0;
+    this.posY = 0;
+
+    this.currentSinValue = 0;
+    this.maxSinValue = Math.PI * 2;
+    this.minSinValue = 0;
+    this.waterCenter = (canvas.height - water.Y0) / 2;
+
+    this.init = function()
+    {
+        this.fin = new Rect(fishColor);
+        this.posX = this.X0 + (Math.random() * water.width);
+        this.currentSinValue = Math.random() * Math.PI * 2;
+        this.posY = this.Y0 + Math.sin(this.currentSinValue) * (this.waterCenter - 5);
+        this.setDirection(Directions.RIGHT);
+        this.eye = new Circle(fishEyeColor, this.eyeSize, this.X0, this.Y0);
+    }
+    
+    this.setDirection = function(direction)
+    {
+         this.direction = direction;
+    }
+
+    this.move = function(){
+        switch(this.direction)
+        {
+            case Directions.RIGHT:
+                this.posX += 1;
+                if(this.posX > canvas.width - (this.bodySize + this.finLength))
+                {
+                    this.posX = canvas.width - this.bodySize - this.finLength;
+                    this.direction = Directions.LEFT;
+                }
+                break;
+            case Directions.LEFT:
+                this.posX -= 1;
+                if(this.posX < water.X0 + this.bodySize + this.finLength)
+                {
+                    this.posX = water.X0 + this.bodySize + this.finLength;
+                    this.direction = Directions.RIGHT;
+                }
+                break;
+        }
+
+        this.currentSinValue += Math.PI / 20;
+
+        if(this.currentSinValue >= this.maxSinValue)
+        {
+            this.currentSinValue = 0;
+        }
+        this.posY = this.Y0 + Math.sin(this.currentSinValue) * (this.waterCenter - this.bodySize);
+    }
+
+    this.draw = function()
+    {
+        switch(this.direction)
+        {
+            case Directions.RIGHT:
+                //draw body
+                context.save();
+                context.beginPath();
+                context.ellipse(this.posX + this.finLength + this.bodySize, this.posY + this.waterCenter, this.bodySize * 1.5, this.bodySize, 0, 0, 2 * Math.PI);
+                context.stroke();
+                context.fillStyle = fishColor;
+                context.fill();
+                context.restore();
+                //draw fin
+                context.save();
+                context.beginPath();
+                context.moveTo(this.posX + this.bodySize, this.posY - (this.finHeight / 2) + this.waterCenter + this.bodySize);
+                context.lineTo(this.posX + this.bodySize - this.finLength, this.posY - this.finHeight + this.waterCenter + this.bodySize);
+                context.lineTo(this.posX + this.bodySize - this.finLength, this.posY + this.waterCenter + this.bodySize);
+                context.fillStyle = fishColor;
+                context.fill();
+                context.restore();
+                //draw eye
+                context.save();
+                context.transform(1,0,0,1,this.posX + this.bodySize - 5, this.posY + (this.bodySize / 2));
+                this.eye.display();
+                context.restore();
+                break;
+            case Directions.LEFT:
+                //draw body
+                 context.save();
+                 context.beginPath();
+                 context.ellipse(this.posX + this.finLength + this.bodySize, this.posY + this.waterCenter, this.bodySize * 1.5, this.bodySize, 0, 0, 2 * Math.PI);
+                 context.stroke();
+                 context.fillStyle = fishColor;
+                 context.fill();
+                 context.restore();
+                 //draw fin
+                 context.save();
+                 context.beginPath();
+                 context.moveTo(this.posX + (2 * this.finLength + this.bodySize), this.posY - (this.finHeight / 2) + this.waterCenter + this.bodySize);
+                 context.lineTo((this.posX + this.finLength) + (2 * this.finLength + this.bodySize), this.posY - this.finHeight + this.waterCenter + this.bodySize);
+                 context.lineTo((this.posX + this.finLength) + (2 * this.finLength + this.bodySize), this.posY + this.waterCenter + this.bodySize);
+                 context.fillStyle = fishColor;
+                 context.fill();
+                 context.restore();
+                 //draw eye
+                 context.save();
+                 context.transform(1,0,0,1,this.posX + this.bodySize - 5, this.posY + (this.bodySize / 2));
+                 this.eye.display();
+                 context.restore();                
+                break;
+        }
+       
+    }
+
+    this.setReferencePoint = function(x,y)
+    {
+        this.X0 = x;
+        this.Y0 = y;
+    }
 }
 
-function Tree()
+function Ship()
 {
-//TODO:Fill with logic
+
 }
 
 function Sun(color, radius)
@@ -182,23 +347,23 @@ function Sun(color, radius)
     this.scaleValue = 1;
     this.shrink = true;
 
-    this.sunRaysWidth = 10;
+    this.sunRaysWidth = 30;
     this.sunRays = [];
 
-    this.initSunRays = function()
-    {
-        for(var i = 0 ; i < sunRaysCount ; i++)
-        {
-            this.sunRays.push(new Rect(sunColor));
-        }
-    }
+    // this.initSunRays = function()
+    // {
+    //     for(var i = 0 ; i < sunRaysCount ; i++)
+    //     {
+    //         this.sunRays.push(new Rect(sunColor));
+    //     }
+    // }
     
     this.init = function()
     {
         this.rX =  35; //top left corner;
         this.rY =  35;
         this.sun = new Circle(this.radius, this.color, this.rX ,this.rY);
-        this.initSunRays();
+        // this.initSunRays();
     }
     
     this.opacityHandler = function()
@@ -227,7 +392,7 @@ function Sun(color, radius)
     {
         if(this.shrink)
         {
-            this.scaleValue -= 0.1;
+            this.scaleValue -= 0.005;
             if(this.scaleValue <= 1)
             {
                 this.scaleValue = 1;
@@ -236,7 +401,7 @@ function Sun(color, radius)
         }
         else
         {
-            this.scaleValue += 0.1;
+            this.scaleValue += 0.005;
             if(this.scaleValue >= sunMaxSize)
             {
                 this.scaleValue = sunMaxSize;
@@ -244,22 +409,22 @@ function Sun(color, radius)
             }
         }
     }
-    this.drawSunRays = function()
-    {
-        context.save();
-        for(var i = 0; i < sunRaysCount; i++)
-        {
-            var sunRay = this.sunRays[i];
-            context.transform(this.sunRaysWidth ,i/sunRaysCount, - (i/sunRaysCount),2,this.rX + 40, this.rY + 40);
-            sunRay.display();
-        }
-        context.restore();
-    }
+    // this.drawSunRays = function()
+    // {
+    //     for(var i = 0; i < sunRaysCount; i++)
+    //     {
+    //         context.save();
+    //         var sunRay = this.sunRays[i];
+    //         context.transform(10 ,0.9,-0.9 ,1,this.rX + 50, (i + 1)*20);
+    //         sunRay.display();
+    //         context.restore();
+    //     }
+    // }
     
     this.draw = function(){
         this.opacityHandler();
         this.sizeHandler();
-        this.drawSunRays();
+        // this.drawSunRays();
         context.save();
         context.globalAlpha = this.opacityValue;
         context.transform(this.scaleValue,0,0,this.scaleValue,this.rX, this.rY);
@@ -314,5 +479,40 @@ function Rect( color )
     this.setReferencePoint = function( x, y ) {
         this.X0 = x;
         this.Y0 = y;
+    }
+}
+//ButtonsEvents
+
+function AddCannonBall()
+{
+    if(pile.height < 4)
+    {
+        pile.setHeight(pile.height + 1);
+    }
+}
+
+function RemoveCannonBall()
+{
+    if(pile.height > 0 )
+    {
+        pile.setHeight(pile.height - 1);
+    }
+}
+
+function AddFish()
+{
+    if(fishCount < 10)
+    {
+        ++fishCount;
+        water.addFish();
+    }
+}
+
+function RemoveFish()
+{
+    if(fishCount > 1)
+    {
+        --fishCount;
+        water.removeFish();
     }
 }
